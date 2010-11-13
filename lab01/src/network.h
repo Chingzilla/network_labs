56f4fd21ee391ee616a72eaaa87d50b208bbf91b
Christopher Ching <chris.e.ching@gmail.com>
2010-11-13 11:41:12 -0700

    Added network header file

---
+++ lab01/src/network.h
@@ -0,0 +1,17 @@
+ 
+/* src/network.h
+ * Used to store common varables for networking parts
+ */
+
+#ifndef NETWORK_H
+#define NETWORK_H
+
+#define	SA	struct sockaddr
+#define SA_IN	struct sockaddr_in
+
+#define	BUFF_SIZE	80
+
+#define PORT_NUM	55555
+#define DEFAULT_IP	"127.0.0.1"
+
+#endif
