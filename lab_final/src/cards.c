04e5d30a2a0b2c2852eb460f9d444687b8bc9bf8
Chris Ching <coolchingster@gmail.com>
2010-12-01 12:31:56 -0700

    Added cardToString method

--- lab_final/src/cards.c
+++ lab_final/src/cards.c
@@ -18,3 +18,57 @@ int countCards(carddeck deck){
 
     return count;
 }
+
+char * cardToString(char * out_string, int card_num){
+    int card_value;
+    int card_suit;
+    char * value[5];
+    char * suit[5];
+
+    //Get card's vaule
+    card_value = (card_num / 13) + 1;
+
+    switch(card_value){
+    case JACK:
+         value = "J";
+         break;
+
+    case QUEEN:
+         vaule = "Q";
+         break;
+
+    case KING:
+         value = "K";
+         break;
+
+    default:
+         itoa(card_value, value, 10);
+         break;
+    }
+         
+
+    //Get card's suit
+    card_suit = card_num % 4;
+
+    switch(card_suit){
+    case SPADES:
+         suit = "S";
+         break;
+    case HEARTS:
+         suit = "H";
+         break;
+    case DIAMONDS:
+         suit = "D";
+         break;
+    case CLUBS:
+         suit = "C";
+         break;
+    default:
+         suit = "NULL";
+    }
+
+    sprintf("%s%s", value, suit);
+    return value;
+}
+
+
