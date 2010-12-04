2907c6dd05633f7dc3e95be8eb156ef1743d5afe
Chris Ching <coolchingster@gmail.com>
2010-12-04 10:04:27 -0700

    Added connection.h

---
+++ lab_final/src/connection/connection.h
@@ -0,0 +1,17 @@
+/*
+ * src/connection/connection.h
+ * Chris Ching
+ */
+
+#ifndef _CONNECTION_H
+#define _CONNECTION_H
+
+#define GAMEPORT 54545
+
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <netinet/tcp.h>
+#include <arpa/inet.h>
+
+#endif //_CONNECTION_H
