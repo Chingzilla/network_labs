a1ff76786f77973a2e4691be471de49dc6e6eed6
Chris Ching <coolchingster@gmail.com>
2010-12-04 12:03:59 -0700

    added more magic values to connection.h

--- lab_final/src/connection/connection.h
+++ lab_final/src/connection/connection.h
@@ -6,12 +6,16 @@
 #ifndef _CONNECTION_H
 #define _CONNECTION_H
 
+#define BROADCASTPORT 54545
 #define GAMEPORT 54545
+#define REQUESTMSG "REQUESTING_GAME"
+#define MAXBUF 128
 
 #include <sys/types.h>
 #include <sys/socket.h>
 #include <netinet/in.h>
 #include <netinet/tcp.h>
 #include <arpa/inet.h>
+#include <netdb.h>
 
 #endif //_CONNECTION_H
